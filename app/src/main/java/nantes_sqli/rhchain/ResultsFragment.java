9aadd3157399b97fb4313a65bdca3bc47b4ac24c
abohssain <abohssain@sqli.com>
2017-01-09 17:43:01 +0100

    histograms bars done

--- app/src/main/java/nantes_sqli/rhchain/ResultsFragment.java
+++ app/src/main/java/nantes_sqli/rhchain/ResultsFragment.java
@@ -3,20 +3,14 @@ package nantes_sqli.rhchain;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
-import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,7 +19,8 @@ import java.util.List;
 public class ResultsFragment extends Fragment {
 
     private BarChart chartQ1;
-    private HorizontalBarChart chartQ2;
+    private BarChart chartQ2;
+    private BarChart chartQ3;
 
     public ResultsFragment() {
     }
@@ -41,39 +36,41 @@ public class ResultsFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        chartQ1 = (BarChart) view.findViewById(R.id.chartQ1);
-        //        chart.setDescription("Q1");
-//        chart.setOnChartValueSelectedListener((OnChartValueSelectedListener) viewGroup);
-//        Log.d("debbug", "onCreateView: chart");
-//        chart.getDescription().setEnabled(false);
+        chartQ1 = (HorizontalBarChart) view.findViewById(R.id.chartQ1);
 
         chartQ2 = (HorizontalBarChart) view.findViewById(R.id.chartQ2);
+
+        chartQ3 = (HorizontalBarChart) view.findViewById(R.id.chartQ3);
 
         mockResultSurveyDemo();
     }
 
-    /**
-     * For demo only
-     */
+                     /**
+                      * For demo only
+                      *
+                      */
+
     private void mockResultSurveyDemo() {
+       ArrayList<Integer> colors = new ArrayList<Integer>();
 
-        ArrayList<Integer> colors = new ArrayList<Integer>();
+//Résultat pour les 3 questions du sondage.
 
-//        Résultat pour les 3 questions du sondage.
         ArrayList<Integer> nbVoteForQuestion1 = new ArrayList<Integer>();
-        nbVoteForQuestion1.addAll(Arrays.asList(2, 5, 3));
+        nbVoteForQuestion1.addAll(Arrays.asList(3,4,6));
 
         ArrayList<Integer> nbVoteForQuestion2 = new ArrayList<Integer>();
-        nbVoteForQuestion2.addAll(Arrays.asList(1, 3, 6));
+        nbVoteForQuestion2.addAll(Arrays.asList(3,4,6));
 
-        int[] nbVoteForQuestion3 = {8, 0, 2};
+        ArrayList<Integer> nbVoteForQuestion3 =new ArrayList<Integer>();
+        nbVoteForQuestion3.addAll(Arrays.asList(2,6,7));
+
 
         BarData barData = new BarData();
-//        La fonction de récupération des pourcentage est à créer
+
+
+//La fonction de récupération des pourcentage est à créer
 
         List<BarEntry> entries = new ArrayList<BarEntry>();
-
-
         colors.add(R.color.unsatisfied);
         colors.add(R.color.neutral);
         colors.add(R.color.satisfied);
@@ -83,6 +80,9 @@ public class ResultsFragment extends Fragment {
 
         barData = createBarChartObjectStacked(nbVoteForQuestion2, colors);
         chartQ2.setData(barData);
+
+        barData = createBarChartObjectStacked(nbVoteForQuestion3, colors);
+        chartQ3.setData(barData);
 
     }
 
@@ -94,29 +94,27 @@ public class ResultsFragment extends Fragment {
      *               been declare in @color
      * @return objet type bardata setted to be display by BarChart object
      */
-    //    Prepare date for classic BarChart
+
+//Prepare date for classic BarChart
     private BarData createBarChartObject(ArrayList<Integer> values, ArrayList<Integer> colors) {
 
         List<BarEntry> entries = new ArrayList<BarEntry>();
 
         for (int i = 0; i < values.size(); i++) {
             entries.add(new BarEntry(((float) i), (float) values.get(i)));
-        }
 
-//        for (int i =0; i<values.size() ; i++) {
-//            entries.add(new BarEntry(values.get(i) ,i+1  ));
-//        }
+
+        }
 
         BarDataSet dataSet = new BarDataSet(entries, "");
+        dataSet.setColors(new int[] {R.color.unsatisfied, R.color.neutral, R.color.satisfied}, getContext());
 
-        for (int i = 0; i < colors.size(); i++) {
-            dataSet.addColor(colors.get(i));
-        }
 
         BarData barData = new BarData(dataSet);
 
         return barData;
     }
+
 
     /**
      * creation of the dataset use by barchart stacked
@@ -126,17 +124,19 @@ public class ResultsFragment extends Fragment {
      *               been declare in @color
      * @return objet type bardata setted to be display by stacked BarChart object
      */
-    //    For Stacked BarChart
+
+
+
+//For Stacked BarChart
+
     private BarData createBarChartObjectStacked(ArrayList<Integer> values, ArrayList<Integer> colors) {
 
         List<BarEntry> entries = new ArrayList<BarEntry>();
-//        for (i =0; i<values.size() ; i++) {
-//            entries.add(new BarEntry(i+1 , values.get(i)));
-//
-//  }
+
         float[] valuesFloat = convertIntToFloat(values);
-        entries.add(new BarEntry(1f, valuesFloat));
         entries.add(new BarEntry(2f, valuesFloat));
+        entries.add(new BarEntry(2f, valuesFloat));
+        entries.add(new BarEntry(5f, valuesFloat));
 
         BarDataSet dataSet = new BarDataSet(entries, "y'a quelque chose qui passe?");
 
@@ -145,7 +145,7 @@ public class ResultsFragment extends Fragment {
         }
 
         BarData barData = new BarData(dataSet);
-
+        dataSet.setColors(new int[] {R.color.unsatisfied, R.color.neutral, R.color.satisfied}, getContext());
         return barData;
     }
 
@@ -155,6 +155,7 @@ public class ResultsFragment extends Fragment {
      * @param arr list of Integer
      * @return array of float
      */
+
     public float[] convertIntToFloat(List<Integer> arr) {
         float[] arrResult = new float[arr.size()];
         for (int i = 0; i < arr.size(); i++) {
@@ -162,8 +163,13 @@ public class ResultsFragment extends Fragment {
         }
         return arrResult;
     }
+
+
+//
 // TODO Import des Results
 // TODO Extraction vote user
 // TODO Calcul pourcentage réponse pour chaque question
 // TODO Affichage icone choix user et camembert
+
+
 }
