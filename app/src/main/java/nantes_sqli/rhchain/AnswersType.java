ccf1c75058d97ee06adf837ce28cc34612ff2514
mikael-came <mikael.came@gmail.com>
2017-02-06 09:37:26 +0100

    refacto bouchonnage

---
+++ app/src/main/java/nantes_sqli/rhchain/AnswersType.java
@@ -0,0 +1,9 @@
+package nantes_sqli.rhchain;
+
+/**
+ * Created by mcame on 02/02/17.
+ */
+
+public enum AnswersType {
+    THREE_ANSWERS
+}
--- app/src/main/java/nantes_sqli/rhchain/Bouchonnage.java
+++ app/src/main/java/nantes_sqli/rhchain/Bouchonnage.java
@@ -2,9 +2,14 @@ package nantes_sqli.rhchain;
 
 import android.util.Log;
 
-import java.sql.CallableStatement;
 import java.util.ArrayList;
+import java.util.List;
+
 import android.support.v7.app.AppCompatActivity;
+
+import nantes_sqli.rhchain.data.Answer;
+import nantes_sqli.rhchain.data.Survey;
+import nantes_sqli.rhchain.data.User;
 
 /**
  * Created by alb on 19/01/17.
@@ -13,82 +18,51 @@ import android.support.v7.app.AppCompatActivity;
 
 public class Bouchonnage extends AppCompatActivity {
 
-    Survey survey = new Survey();
-    User user = new User();
-    BallotBox ballotbox = new BallotBox();
+    static String[] questionsStr = {
+        "Êtes-vous satisfait(e) de l'ambiance de travail au sein de l'agence ?",
+        "Êtes-vous satisfait(e) de votre mission actuelle ?",
+        "Êtes-vous satisfait(e) des informations qui vous sont transmises concernant la situation et les perspectives de l'agence et du groupe ?"
+    };
 
-    public Bouchonnage(Survey survey) {
-        this.survey = survey;
-    }
 
     public Bouchonnage() {
     }
 
-    public Survey setDemoSurvey() {
-
+    public static Survey setDemoSurvey() {
+        Survey survey = new Survey();
         Log.v("méthode", "importation du survey");
         survey.setId("Questionnaire_RH_demo");
 
-        Answer satisfied = new Answer();
-        Answer verySatisfied = new Answer();
-        Answer unsatisfied = new Answer();
+        //Construction du type de reponse 3 choix
+        List<Answer> threeChoicesAnswers = AnswersFactory.createAnswers(AnswersType.THREE_ANSWERS);
 
-        ArrayList<Answer> answers = new ArrayList<>();
-
-
-        unsatisfied.setId("1");
-        unsatisfied.setDescription(getResources().getString(R.string.descr_btn_unsatisfied));
-        unsatisfied.setValue(0);
-        unsatisfied.setNameImage(String.valueOf(R.drawable.ic_sentiment_dissatisfied_black_24dp));
-        satisfied.setId("2");
-        satisfied.setDescription(getResources().getString(R.string.descr_btn_neutral));
-        satisfied.setValue(1);
-        satisfied.setNameImage(String.valueOf(R.drawable.ic_sentiment_neutral_black_24dp));
-        verySatisfied.setId("3");
-        verySatisfied.setDescription(getResources().getString(R.string.descr_btn_satisfied));
-        verySatisfied.setValue(2);
-        verySatisfied.setNameImage(String.valueOf(R.drawable.ic_sentiment_satisfied_black_48dp));
-
-        answers.add(unsatisfied);
-        answers.add(satisfied);
-        answers.add(verySatisfied);
-
-        Question question1 = new Question();
-        Question question2 = new Question();
-        Question question3 = new Question();
-
+        //Construction des questions
         ArrayList<Question> questions = new ArrayList<>();
-        question1.setId("1");
-//        question1.setTextQuestion(getResources().getString(R.string.txt_question1));
-        question1.setTextQuestion(getResources().getString(R.string.txt_question1));
-        question1.setAnswers(answers);
 
-        question2.setId("2");
-        question2.setTextQuestion(getResources().getString(R.string.txt_question2));
-        question2.setAnswers(answers);
+        // Construction des questions
+        for(int i=0;i<questions.size();i++) {
+            Question question = new Question(String.valueOf(i), questionsStr[i], threeChoicesAnswers);
+            questions.add(question);
 
-        question3.setId("3");
-        question3.setTextQuestion(getResources().getString(R.string.txt_question3));
-        question3.setAnswers(answers);
-
-        questions.add(question1);
-        questions.add(question2);
-        questions.add(question3);
+        }
 
         survey.setId("1");
-        survey.setLabel(getResources().getString(R.string.txt_ttl_survey));
+        survey.setLabel("Satisfaction travail");
         survey.setQuestions(questions);
 
         return survey;
     }
 
-    public User setDemoUser(){
+    public static User setDemoUser(){
+        User user = new User();
         user.setIdentity("toto");
         return user;
     }
 
-    public BallotBox setDemoBallotBox() {
+    public static BallotBox setDemoBallotBox() {
 
+
+        BallotBox ballotbox = new BallotBox();
         return ballotbox;
     }
 }
