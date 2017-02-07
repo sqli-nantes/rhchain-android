da6513887bc9bf1e1adc85d3af0b58d9968c7fa0
mikael-came <mikael.came@gmail.com>
2017-02-07 13:45:20 +0100

    recactoring +  gestion des reponses et affichage boutons soumission
    Refactoring de l'initialisation de l'écran
    Gestion des reponses données en array, porté par Results.
    activation dynamique du bouton de soummission. s'active quand l'utilisateur a renseigné toutes les questions

---
+++ app/src/test/java/nantes_sqli/rhchain/SurveysFragmentTest.java
@@ -0,0 +1,16 @@
+package nantes_sqli.rhchain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mcame on 07/02/17.
+ */
+public class SurveysFragmentTest {
+    @Test
+    public void isSurveyCompleted() throws Exception {
+
+    }
+
+}
