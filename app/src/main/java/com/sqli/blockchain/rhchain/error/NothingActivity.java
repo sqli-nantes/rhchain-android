4ed19704c30b2237669e8cdda3c9fc49d4f695cb
Nigui <gunicolas@sqli.com>
2017-09-15 18:08:35 +0200

    refactor: move nothing_activity textview from layout.xml to activity.java

--- app/src/main/java/com/sqli/blockchain/rhchain/error/NothingActivity.java
+++ app/src/main/java/com/sqli/blockchain/rhchain/error/NothingActivity.java
@@ -3,6 +3,7 @@ package com.sqli.blockchain.rhchain.error;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.sqli.blockchain.rhchain.R;
 import com.sqli.blockchain.rhchain.RHChainAbstractActivity;
@@ -17,7 +18,9 @@ import io.reactivex.disposables.Disposable;
 
 public class NothingActivity extends RHChainAbstractActivity {
 
+    private static final String ERROR_MSG = "Oups, ceci n'est pas une erreur mais il semble qu'il n'y ait pas de sondage en ce moment...";
 
+    TextView errorTextview;
     Disposable openedSubscription;
 
     @Override
@@ -25,6 +28,8 @@ public class NothingActivity extends RHChainAbstractActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nothing_activity);
 
+        errorTextview = (TextView) findViewById(R.id.error_textview);
+        errorTextview.setText(ERROR_MSG);
 
         openedSubscription = application.blockchainAPI.registerOpenedEvent()
             .observeOn(AndroidSchedulers.mainThread())
